89407745b3171d30ea8f9a919dad43038333cd42
Haseeb <haseebzemail@gmail.com>
2022-10-01 22:20:36 +0500

    done till q2

---
+++ script.js
@@ -0,0 +1,9 @@
+function question_2() {
+    var name = document.getElementById('q1_name').value;
+    document.getElementById('q1_result').innerText=`Hi ${name}, Welcome to Alright Tech! Would you like to have some tea?`;
+
+}
+
+function run_script() {
+    alert("Js file is working");
+}
